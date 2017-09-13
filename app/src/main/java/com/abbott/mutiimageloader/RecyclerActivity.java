9a517b896851a4e52343b0bed4507ced0805d2fe
jinyabo <407007276@qq.com>
2017-09-13 22:00:10 +0800

    实现列表显示数据

--- app/src/main/java/com/abbott/mutiimageloader/RecyclerActivity.java
+++ app/src/main/java/com/abbott/mutiimageloader/RecyclerActivity.java
@@ -1,11 +1,14 @@
 package com.abbott.mutiimageloader;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.abbott.mutiimgloader.circularavatar.CircularImageView;
 import com.abbott.mutiimgloader.util.ImageLoader;
@@ -13,22 +16,12 @@ import com.abbott.mutiimgloader.util.ImageLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class MainActivity extends Activity {
-
-    ArrayList<Bitmap> mBmps1 = new ArrayList<Bitmap>();
-    ArrayList<Bitmap> mBmps2 = new ArrayList<Bitmap>();
-    ArrayList<Bitmap> mBmps3 = new ArrayList<Bitmap>();
-    ArrayList<Bitmap> mBmps4 = new ArrayList<Bitmap>();
-    ArrayList<Bitmap> mBmps5 = new ArrayList<Bitmap>();
-
-    CircularImageView mImageView1;
-    CircularImageView mImageView2;
-    CircularImageView mImageView3;
-    CircularImageView mImageView4;
-    CircularImageView mImageView5;
+public class RecyclerActivity extends Activity {
+    RecyclerView rv;
+    List<List<String>> mDatas = new ArrayList<>();
 
     static List<String> urls = new ArrayList<>();
+    ImageLoader imageLoader;
 
     static {
         urls.add("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1505294718&di=b6934dd570c0c6962a8dbbb12eac27f5&src=http://www.zhlzw.com/UploadFiles/Article_UploadFiles/201204/20120412123914329.jpg");
@@ -49,79 +42,82 @@ public class MainActivity extends Activity {
         urls.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1505317488354&di=996c983ba9cd30bf04b6bd684b0167ce&imgtype=0&src=http%3A%2F%2Fs6.sinaimg.cn%2Fmiddle%2F10bc65e44c8d713456d55%26960");
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recycler);
 
-        // test data
-        Bitmap avatar1 = BitmapFactory.decodeResource(getResources(), R.drawable.headshow1);
-        Bitmap avatar2 = BitmapFactory.decodeResource(getResources(), R.drawable.headshow2);
-        Bitmap avatar3 = BitmapFactory.decodeResource(getResources(), R.drawable.headshow3);
-        Bitmap avatar4 = BitmapFactory.decodeResource(getResources(), R.drawable.headshow4);
-        Bitmap avatar5 = BitmapFactory.decodeResource(getResources(), R.drawable.headshow5);
+        imageLoader = new ImageLoader(this);
+        rv = (RecyclerView) findViewById(R.id.rv);
 
-        mBmps1.add(avatar1);
-        mBmps2.add(avatar1);
-        mBmps3.add(avatar1);
-        mBmps4.add(avatar1);
-        mBmps5.add(avatar1);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerAdapter adapter = new RecyclerAdapter();
+        rv.setAdapter(adapter);
 
-        mBmps2.add(avatar2);
-        mBmps3.add(avatar2);
-        mBmps4.add(avatar2);
-        mBmps5.add(avatar2);
 
-        mBmps3.add(avatar3);
-        mBmps4.add(avatar3);
-        mBmps5.add(avatar3);
 
-        mBmps4.add(avatar4);
-        mBmps5.add(avatar4);
+        initData();
 
-        mBmps5.add(avatar5);
+        adapter.notifyDataSetChanged();
 
-        mImageView1 = (CircularImageView) findViewById(R.id.circularImageView1);
-        mImageView2 = (CircularImageView) findViewById(R.id.circularImageView2);
-        mImageView3 = (CircularImageView) findViewById(R.id.circularImageView3);
-        mImageView4 = (CircularImageView) findViewById(R.id.circularImageView4);
-        mImageView5 = (CircularImageView) findViewById(R.id.circularImageView5);
-
-        mImageView1.setImageBitmaps(mBmps1);
-        mImageView2.setImageBitmaps(mBmps2);
-        mImageView3.setImageBitmaps(mBmps3);
-        mImageView4.setImageBitmaps(mBmps4);
-        mImageView5.setImageBitmaps(mBmps5);
-
-//        new ImageLoader(this).displayImages(urls,mImageView1,200,200);
-
-        CircularImageView mImageView21 = (CircularImageView) findViewById(R.id.circularImageView21);
-        CircularImageView mImageView22 = (CircularImageView) findViewById(R.id.circularImageView22);
-        CircularImageView mImageView23 = (CircularImageView) findViewById(R.id.circularImageView23);
-        CircularImageView mImageView24 = (CircularImageView) findViewById(R.id.circularImageView24);
-        CircularImageView mImageView25 = (CircularImageView) findViewById(R.id.circularImageView25);
-
-        mImageView21.setImageBitmaps(mBmps1);
-        mImageView22.setImageBitmaps(mBmps2);
-        mImageView23.setImageBitmaps(mBmps3);
-        mImageView24.setImageBitmaps(mBmps4);
-        mImageView25.setImageBitmaps(mBmps5);
-
-        CircularImageView mImageView31 = (CircularImageView) findViewById(R.id.circularImageView31);
-        CircularImageView mImageView32 = (CircularImageView) findViewById(R.id.circularImageView32);
-        CircularImageView mImageView33 = (CircularImageView) findViewById(R.id.circularImageView33);
-        CircularImageView mImageView34 = (CircularImageView) findViewById(R.id.circularImageView34);
-        CircularImageView mImageView35 = (CircularImageView) findViewById(R.id.circularImageView35);
-
-        mImageView31.setImageBitmaps(mBmps1);
-        mImageView32.setImageBitmaps(mBmps2);
-        mImageView33.setImageBitmaps(mBmps3);
-        mImageView34.setImageBitmaps(mBmps4);
-        mImageView35.setImageBitmaps(mBmps5);
 
     }
 
-    public void onBitmapBtnClick(View view) {
-        startActivity(new Intent(this, RecyclerActivity.class));
+    private void initData() {
+        for (int i = 0; i < 100; i++) {
+            int j = (int) (Math.random() * 6);
+
+            if (j == 0) {
+                j = 1;
+            }
+            List<String> arrayUrls = new ArrayList<>();
+            for (int m = 0; m < j; m++) {
+                int position = (int) (Math.random() * urls.size());
+                arrayUrls.add(urls.get(position));
+
+            }
+
+            mDatas.add(arrayUrls);
+        }
+
+
     }
+
+
+    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    RecyclerActivity.this).inflate(R.layout.item_head, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            List<String> urls = mDatas.get(position);
+
+            imageLoader.displayImages(urls, holder.imageView, 200, 200);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+            CircularImageView imageView;
+
+            public MyViewHolder(View view) {
+                super(view);
+                imageView = (CircularImageView) view.findViewById(R.id.imageView);
+            }
+        }
+    }
+
 }
